2ef43d37a50bdae59720d82810d9ed887a72bfc7
jampierebt <jampiere.berrio@interseguro.com.pe>
2024-06-25 16:42:09 -0500

    change file

--- build.sh
+++ build.sh
@@ -1,21 +1,3 @@
-
-# ------------------------------------
-# generate zip vm files
-# ------------------------------------
-
-#zip -r "ODS_UAT.zip" "Desarrollo/Replicacion_ods/ODS_UAT"
-
-#find . -name "desktop.ini" -type f -delete
-
-# ------------------------------------
-# python de replicar
-# ------------------------------------
-
-#python Framework/ejecucion_jobs_validacion.py
-#python Framework/ejecucion_replicacion_ods.py
-#python Framework/ejecucion_replicacion_sp.py
-#python Framework/ejecucion_query_validacion.py
-
 # ------------------------------------
 # Subir a GIT repository
 # ------------------------------------
