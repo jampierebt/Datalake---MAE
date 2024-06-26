186395442d4b539b4707097429a9563c722afa62
jampierebt <jampiere.berrio@interseguro.com.pe>
2024-06-26 14:44:54 -0500

    change file

---
+++ Desarrollo/Replicacion_insert/CLAIM_raw.sql
@@ -0,0 +1,2 @@
+INSERT INTO iter-data-storage-pv-uat.acsele_alloy_new.CLAIM_raw
+SELECT * FROM iter-data-storage-pv-uat.acsele_temp.CLAIM_raw
---
+++ Desarrollo/Replicacion_insert/COBERTURA_raw.sql
@@ -0,0 +1,2 @@
+INSERT INTO iter-data-storage-pv-uat.acsele_alloy_new.COBERTURA_raw
+SELECT * FROM iter-data-storage-pv-uat.acsele_temp.COBERTURA_raw
